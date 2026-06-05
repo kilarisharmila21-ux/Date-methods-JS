fa376dd65a3c9e26af9e260ff3eddf390857c0c8
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-06-05 22:32:40 +0530

    programs for working with dates, time etc...,
    JavaScript Date methods with practical examples and date operations.

---
+++ datemethods.js
@@ -0,0 +1,29 @@
+// getDate                               // setDate
+// getMonth                              // setMonth
+// getFullYear                           // setFullYear
+// getHours                              // setHours
+// getMinutes                            // setMinutes
+// getSeconds                            // setSeconds
+
+
+
+// getDate
+
+const toDay = new Date()
+
+console.log(toDay)
+
+// getMonth = ( add +1 because index starts with 0 )
+
+const toDayy = new Date()
+
+console.log(toDayy.getMonth() + 1)
+
+
+// setDate
+
+var toDayyy = new Date()
+
+toDayyy.setDate(10)
+
+console.log(toDayyy)
